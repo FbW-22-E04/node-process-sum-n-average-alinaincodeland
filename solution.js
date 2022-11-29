1ab0c61acc503bfcfd2fbaa0d225f974b0e0cc3e
alinaincodeland <alinacuznetov@gmail.com>
2022-11-29 15:34:21 +0100

    solution addd

--- solution.js
+++ solution.js
@@ -1,13 +1,27 @@
 const input = process.argv.slice(2);
+
 let result = 0;
-for (let i = 0; i < input.length; i++) {
-  result = result + Number(input[i]);
+
+for (let i = 1; i < input.length; i++) {
+  let userInput = Number(input[i]);
+
   if (isNaN(input[i])) {
     console.log(
-      `Sorry, the argument ${input[i]} is not a number, please try again`
+      `Sorry, the argument ${userInput} is not a number, please try again`
     );
     return;
   }
-  result = result + Number(input[i]);
+  result = result + userInput;
 }
-console.log(result);
+
+let average = result / (input.length - 1);
+
+if (input[0] === "sum") {
+  console.log(result);
+} else if (input[0] === "avg") {
+  console.log(average);
+} else {
+  console.log(
+    'I cannot calculate that, please type either "sum" (to calculate the sum) or "avg" (To calculate the Average)'
+  );
+}
